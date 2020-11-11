03ccb1dcd0cc1eec6ea13d0532bf63ffbfad5c08
William Soukkachang <Wsoukkachang@yahoo.com>
2020-11-11 13:07:50 -0600

    Updated header. Added drop down menu. Connected to Disease.sh API to get country data.

--- covid19tracker/src/App.js
+++ covid19tracker/src/App.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(["worldwide"]);
+
+  //USESTATE = CREATE VARIABLE IN JSX
+  //USEEFFECT = PULL DATA FROM API
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          //
+          setCountries(countries);
+        });
+    };
+
+    // call function
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    //when you click on a country in drop down menu
+    const countryCode = event.target.value;
+
+    console.log("HEYYYYY >>>>>", countryCode);
+
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>COVID 19 Tracker</h1>
+      <div className="app__header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+
+            {/* <MenuItem value="worldwide">Worldwide1</MenuItem>
+            <MenuItem value="worldwide">Worldwide2</MenuItem>
+            <MenuItem value="worldwide">Worldwide4</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
       {/* Header */}
       {/* Title + Dropdown */}
-      
+
       {/* Infoboxs */}
       {/* Infoboxs */}
       {/* Infoboxs */}
